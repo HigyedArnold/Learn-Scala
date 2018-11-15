20716049f990e067a6574874be101df36dfc071e
Arnold Higyed <arnold.higyed@softnrg.ro>
2018-11-15 17:34:29 +0200

    First up!

---
+++ src/main/scala/com/allaboutscala/chapter/one/Introduction.scala
@@ -0,0 +1,20 @@
+package com.allaboutscala.chapter.one
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * Created by ArnoldHigyed on 14/11/2018
+  */
+object Introduction extends App with LazyLogging {
+
+  println("\n//-----------------------------------------------------------------------------------------------------\n")
+
+  println("Hello World from Scala!")
+  println("Command line arguments are: ")
+  println(args.mkString(", "))
+
+  logger.info("Hello World from Scala Logging!")
+
+  println("\n//-----------------------------------------------------------------------------------------------------\n")
+
+}
