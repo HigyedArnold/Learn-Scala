ce6e1cce26f275eebec0212fb20143071153e2e4
Arnold Higyed <arnold.higyed@softnrg.ro>
2018-12-07 10:19:14 +0200

    Adding fpinscala: GettingStarted and DataStructures chapter.

--- src/main/scala/com/allaboutscala/akka/fsm/DonutBakingActorFSMTests.scala
+++ src/main/scala/com/allaboutscala/akka/fsm/DonutBakingActorFSMTests.scala
@@ -2,8 +2,9 @@ package com.allaboutscala.akka.fsm
 
 import akka.actor.ActorSystem
 import akka.testkit.{DefaultTimeout, ImplicitSender, TestFSMRef, TestKit}
-import com.allaboutscala.akka.fsm.LoggingFSM.{BakingData, BakingStates, DonutBakingActor, Stop}
+import com.allaboutscala.akka.fsm.LoggingFSM._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import scala.concurrent.duration._
 
 /**
   * Created by ArnoldHigyed on 06/12/2018
@@ -25,6 +26,14 @@ class DonutBakingActorFSMTests
   "DonutBakingActor" should {
     "have initial state of BakingStates.Stop" in {
       donutBakingActorFSM.stateName shouldEqual Stop
+    }
+  }
+
+  "DonutBakingActor" should {
+    "process BakeDonut event and switch to the BakingStates.Start state" in {
+      donutBakingActorFSM ! BakeDonut
+      // test the state using awaitCond
+      awaitCond(donutBakingActorFSM.stateName == Start, 2 second, 1 second)
     }
   }
 
